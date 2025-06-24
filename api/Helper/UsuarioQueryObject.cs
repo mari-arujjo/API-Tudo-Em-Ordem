7999db81ddf45ffd05e23876dad5033944ebc9c7
Mariana Araújo <mariana.estudo9889@gmail.com>
2025-06-24 12:02:02 -0300

    Retirando paginação

--- api/Helper/UsuarioQueryObject.cs
+++ api/Helper/UsuarioQueryObject.cs
@@ -9,7 +9,7 @@
         public int? nivel_acesso { get; set; } = null;
         public string? ordenar { get; set; } = null;
         public bool DESC { get; set; } = false;
-        public int num_pag { get; set; } = 1; //pag atual -> começa em 1
-        public int tam_pag { get; set; } = 2; // qnt de elemntos por pag
+        //public int num_pag { get; set; } = 1; //pag atual -> começa em 1
+        //public int tam_pag { get; set; } = 2; // qnt de elemntos por pag
     } 
 }
