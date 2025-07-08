2b3408fede58f9a664ddc55b501c8ec9859a004c
Mariana <120474044+mari-arujjo@users.noreply.github.com>
2025-07-08 17:18:48 -0300

    aaaaaa

--- api/Controllers/UsuarioController.cs
+++ api/Controllers/UsuarioController.cs
@@ -60,7 +60,6 @@ namespace api.Controllers
                 return BadRequest(ModelState);
 
             var userModel = dto.CriarNovoUsuarioDto();
-            userModel.foto_url ??= "https://fldoyzuifdwwxrtzinsb.supabase.co/storage/v1/object/sign/fotos-de-perfil/sem-foto.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV82MDEyZDE1Mi1lMDAwLTQ3NDUtYmQ3Zi1iOTI5OTZkMTlkMWYiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJmb3Rvcy1kZS1wZXJmaWwvc2VtLWZvdG8ucG5nIiwiaWF0IjoxNzUxNjM2NjQyLCJleHAiOjE3ODMxNzI2NDJ9.7TuEkC9FKlqyJBGwrYKYZMobIDV-fm042xpb0JtLv8k";
             await _usuarioRep.CreateAsync(userModel);
 
             return CreatedAtAction(nameof(ObterPorID), new { id = userModel.id_usuario }, userModel.ConverterParaUsuarioDto());
