8186319710120bffa953f1697bb34ac3a379ba51
Mariana Araújo Silva <mariana.estudo9889@gmail.com>
2025-07-08 12:49:18 -0300

    Atualização geral

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a2c36e037f55f484ad4aa0395adee52daf4e4d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7583b88aea36fc7a82cc34bf6b7c7bf97285c4fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
