7fb344616ed5f42ccd26ce760c9dedf63a89bac6
Mariana Araújo <mariana.estudo9889@gmail.com>
2025-07-09 09:18:19 -0300

    Melhorando mensagens de erro

--- api/Dtos/AtualizarUsuarioDto.cs
+++ api/Dtos/AtualizarUsuarioDto.cs
@@ -7,16 +7,18 @@ namespace api.Dtos
         //public int id_usuario { get; set; }
 
         [Required]
-        [MinLength(5, ErrorMessage = "O username deve ter no mínimo 5 carcateres")]
-        [MaxLength(15, ErrorMessage = "Limite de carcateres: 15")]
+        [MinLength(5, ErrorMessage = "O username deve ter no mínimo 5 caracteres")]
+        [MaxLength(15, ErrorMessage = "Limite de caracteres: 15")]
         public string usuario { get; set; } = string.Empty;
 
 
         [Required]
-        [MinLength(10, ErrorMessage = "O nome deve ter no mínimo 10 carcateres")]
-        [MaxLength(50, ErrorMessage = "Limite de carcateres: 50")]
+        [MinLength(10, ErrorMessage = "O nome deve ter no mínimo 10 caracteres")]
+        [MaxLength(50, ErrorMessage = "Limite de caracteres: 50")]
         public string nome { get; set; } = string.Empty;
+
         public int nivel_acesso { get; set; }
+
         public string foto_url { get; set; } = string.Empty;
 
     }
